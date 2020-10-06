682ffcd7be524576c2bfbcf4aac12107f5dc6cd9
carltonprananda <37216812+carltonprananda@users.noreply.github.com>
2020-10-06 08:45:50 +0800

    Mission 1 in Progress

--- belajar_flutterdean1/lib/home.dart
+++ belajar_flutterdean1/lib/home.dart
@@ -5,34 +5,62 @@ class HomePart extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Hotel App',
+      theme: ThemeData(brightness: Brightness.light),
       home: Scaffold(
         appBar: AppBar(
           title: Text('Hotel'),
           backgroundColor: Colors.blueAccent,
         ),
-        body: Column(
-          children: <Widget>[
-            Image.network(
-                "https://ik.imagekit.io/tvlk/apr-asset/dgXfoyh24ryQLRcGq00cIdKHRmotrWLNlvG-TxlcLxGkiDwaUSggleJNPRgIHCX6/hotel/asset/20021087-c9dd5ef476a9c7766ad7b24ebb347e30.jpeg?tr=q-40,c-at_max,w-740,h-500&_src=imagekit"),
-            Row(),
-            Text(
-              'Welcome to Golden Tulip Holland Batu',
-              style: TextStyle(fontSize: 24, fontFamily: "Sans Serif"),
-            ),
-            Text(
-              'entertainments, it offers two food & beverafe venues, swimming pools, a spa, a fitness center, a kids playground and meeting room facilities which cater up to 2009 person',
-              style: TextStyle(fontSize: 16, fontFamily: "Sans Serif"),
-              textAlign: TextAlign.left,
-            ),
-            Text(
-              'Golden Tulip Holland Resort offers the kind of facilities and services that make travelling with children a breeze. The hotel is geared towards the needs and requirements of all families, big and small. Our Hotel offers various activities for kids, along with a pool/slide.',
-              style: TextStyle(fontSize: 16, fontFamily: "Sans Serif"),
-              textAlign: TextAlign.left,
-            ),
-            Text(
-              'Try our '
-            )
-          ],
+        body: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Image.network(
+                  "https://ik.imagekit.io/tvlk/apr-asset/dgXfoyh24ryQLRcGq00cIdKHRmotrWLNlvG-TxlcLxGkiDwaUSggleJNPRgIHCX6/hotel/asset/20021087-c9dd5ef476a9c7766ad7b24ebb347e30.jpeg?tr=q-40,c-at_max,w-740,h-500&_src=imagekit"),
+              Row(
+                children: <Widget>[
+                  Image.network(
+                    "https://ik.imagekit.io/tvlk/apr-asset/dgXfoyh24ryQLRcGq00cIdKHRmotrWLNlvG-TxlcLxGkiDwaUSggleJNPRgIHCX6/hotel/asset/20021087-c9dd5ef476a9c7766ad7b24ebb347e30.jpeg?tr=q-40,c-at_max,w-740,h-500&_src=imagekit",
+                    width: 100,
+                    height: 100,
+                  ),
+                  Image.network(
+                    "https://ik.imagekit.io/tvlk/apr-asset/dgXfoyh24ryQLRcGq00cIdKHRmotrWLNlvG-TxlcLxGkiDwaUSggleJNPRgIHCX6/hotel/asset/20021087-c9dd5ef476a9c7766ad7b24ebb347e30.jpeg?tr=q-40,c-at_max,w-740,h-500&_src=imagekit",
+                    width: 100,
+                    height: 100,
+                  ),
+                  Image.network(
+                    "https://ik.imagekit.io/tvlk/apr-asset/dgXfoyh24ryQLRcGq00cIdKHRmotrWLNlvG-TxlcLxGkiDwaUSggleJNPRgIHCX6/hotel/asset/20021087-c9dd5ef476a9c7766ad7b24ebb347e30.jpeg?tr=q-40,c-at_max,w-740,h-500&_src=imagekit",
+                    width: 100,
+                    height: 100,
+                  ),
+                  Image.network(
+                    "https://ik.imagekit.io/tvlk/apr-asset/dgXfoyh24ryQLRcGq00cIdKHRmotrWLNlvG-TxlcLxGkiDwaUSggleJNPRgIHCX6/hotel/asset/20021087-c9dd5ef476a9c7766ad7b24ebb347e30.jpeg?tr=q-40,c-at_max,w-740,h-500&_src=imagekit",
+                    width: 100,
+                    height: 100,
+                  ),
+                ],
+              ),
+              Text(
+                'Welcome to Golden Tulip Holland Batu',
+                style: TextStyle(fontSize: 24, fontFamily: "Sans Serif"),
+              ),
+              Text(
+                'entertainments, it offers two food & beverafe venues, swimming pools, a spa, a fitness center, a kids playground and meeting room facilities which cater up to 2009 person \n',
+                style: TextStyle(fontSize: 16, fontFamily: "Sans Serif"),
+                textAlign: TextAlign.left,
+              ),
+              Text(
+                'Golden Tulip Holland Resort offers the kind of facilities and services that make travelling with children a breeze. The hotel is geared towards the needs and requirements of all families, big and small. Our Hotel offers various activities for kids, along with a pool/slide. \n',
+                style: TextStyle(fontSize: 16, fontFamily: "Sans Serif"),
+                textAlign: TextAlign.left,
+              ),
+              Text(
+                'Try our tempting menu of drinks and light meals available 24 hours. When you feel the need of food and beverage, we are ready to serve you. Our business center offers computer use and internet services, ',
+                style: TextStyle(fontSize: 16, fontFamily: "Sans Serif"),
+                textAlign: TextAlign.left,
+              )
+            ],
+          ),
         ),
       ),
     );
